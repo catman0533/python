b9f4f5043881b51305f1d15cca077be80cb19ee2
yuriy <catman0533@gmail.com>
2023-05-25 22:37:10 +0300

    Добавлвено решение в task10.py

---
+++ task10.py
@@ -0,0 +1,17 @@
+
+N = int(input('Введите количество монет '))
+orel = 0
+reshka = 0
+for i in range(N):
+    x = int(input('Орел(1) или решка(0)? '))
+    if x == 1:
+        orel += 1
+    else:
+        reshka += 1
+if orel < reshka:
+    print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+elif orel == reshka:
+    print(f'Количество орлов и решек одинаково, по {orel} штук')
+else:
+    print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
+    
